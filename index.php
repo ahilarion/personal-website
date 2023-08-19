35dbec3573aa45f3d2e2f837be04a21c629b9e75
Alan Hilarion <alan.hilarion@proton.me>
2023-08-19 02:24:32 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en" class="h-full bg-gray-800">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/x-icon" href="src/img/profil.jpg">
+    <title>Alan Hilarion</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="w-full min-h-full flex justify-center items-center">
+    <main class="relative p-4 mt-4 mb-4 z-10 w-1/4 select-none bg-[#231E39] rounded-xl min-w-[380px]">
+        <div class="h-[70%] w-full flex flex-col justify-evenly items-center">
+            <img class="h-48 p-2 rounded-full border-4 border-[#15aabf] m-2" src="src/img/profil.jpg">
+            <h1 class="text-white text-xl font-medium ">Alan Hilarion</h1>
+            <h2 class="text-white text-xl font-medium mt-2">Margny-lès-Compiègne - 60280</h2>
+            <h2 class="text-white text-xl mt-2">Student - Full stack web developer</h2>
+            <section class="grid grid-flow-col justify-stretch w-full gap-2 pt-4 pb-4">
+                <button onclick="window.location.href = 'mailto:contact@alan-hilarion.fr'" class="p-3 bg-[#15aabf] rounded text-white hover:bg-[#0c8599]">Message me !</button>
+                <button onclick="window.location.href='https://github.com/ahilarion'" class="p-3 bg-[#15aabf] rounded text-white hover:bg-[#0c8599]">Github</button>
+                <button onclick="window.location.href='resume.php'" class="p-3 bg-[#15aabf] rounded text-white hover:bg-[#0c8599]">My resume</button>
+            </section>
+        </div>
+        <div class="p-4 w-full flex bg-[#1F1A36] rounded-xl flex-wrap content-start gap-2">
+            <h3 class="w-full text-white text-l font-bold ">Skills :</h3>
+            <p class="p-2 hover:bg-white hover:text-gray-900 cursor-pointer text-white text-l border border-gray-400">C++</p>
+            <p class="p-2 hover:bg-white hover:text-gray-900 cursor-pointer text-white text-l border border-gray-400">HTML5</p>
+            <p class="p-2 hover:bg-white hover:text-gray-900 cursor-pointer text-white text-l border border-gray-400">PHP</p>
+            <p class="p-2 hover:bg-white hover:text-gray-900 cursor-pointer text-white text-l border border-gray-400">QT</p>
+            <p class="p-2 hover:bg-white hover:text-gray-900 cursor-pointer text-white text-l border border-gray-400">CSS3</p>
+            <p class="p-2 hover:bg-white hover:text-gray-900 cursor-pointer text-white text-l border border-gray-400">LARAVEL</p>
+            <p class="p-2 hover:bg-white hover:text-gray-900 cursor-pointer text-white text-l border border-gray-400">TAILWINDCSS</p>
+            <p class="p-2 hover:bg-white hover:text-gray-900 cursor-pointer text-white text-l border border-gray-400">MYSQL</p>
+            <p class="p-2 hover:bg-white hover:text-gray-900 cursor-pointer text-white text-l border border-gray-400">FIGMA</p>
+        </div>
+    </main>
+    <svg class="fixed z-0 top-0 h-full w-full" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.dev/svgjs" width="1440" height="560" preserveAspectRatio="none" viewBox="0 0 1440 560"><g mask="url(&quot;#SvgjsMask1002&quot;)" fill="none"><rect width="1440" height="560" x="0" y="0" fill="url(&quot;#SvgjsLinearGradient1003&quot;)"></rect><path d="M1440 0L920.89 0L1440 255.06z" fill="rgba(255, 255, 255, .1)"></path><path d="M920.89 0L1440 255.06L1440 276.37L549.5899999999999 0z" fill="rgba(255, 255, 255, .075)"></path><path d="M549.5899999999999 0L1440 276.37L1440 436.33000000000004L464.4999999999999 0z" fill="rgba(255, 255, 255, .05)"></path><path d="M464.4999999999999 0L1440 436.33000000000004L1440 499.53000000000003L229.0499999999999 0z" fill="rgba(255, 255, 255, .025)"></path><path d="M0 560L584.84 560L0 345.06z" fill="rgba(0, 0, 0, .1)"></path><path d="M0 345.06L584.84 560L629.6500000000001 560L0 322.78z" fill="rgba(0, 0, 0, .075)"></path><path d="M0 322.78L629.6500000000001 560L688.6500000000001 560L0 298.04999999999995z" fill="rgba(0, 0, 0, .05)"></path><path d="M0 298.05L688.6500000000001 560L904.46 560L0 156.47z" fill="rgba(0, 0, 0, .025)"></path></g><defs><mask id="SvgjsMask1002"><rect width="1440" height="560" fill="#ffffff"></rect></mask><linearGradient x1="15.28%" y1="-39.29%" x2="84.72%" y2="139.29%" gradientUnits="userSpaceOnUse" id="SvgjsLinearGradient1003"><stop stop-color="#0e2a47" offset="0"></stop><stop stop-color="#00459e" offset="1"></stop></linearGradient></defs></svg>
+</body>
+</html>
